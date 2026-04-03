df76a53ae27722cdd57ced432d3b2fc32282b8d6
Kavya <kavyak0314@gmail.com>
2026-04-03 21:13:11 +0530

    Next changes

--- app.py
+++ app.py
@@ -17,6 +17,13 @@ def calculator():
         elif operation == "mul":
             result = num1 * num2
         elif operation == "div":
-            result = num1 / num2
+            if num2 != 0:
+                result = num1/num2
+            else:
+                result = "Cannot divide by zero"
+
 
     return render_template("index.html", result=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
